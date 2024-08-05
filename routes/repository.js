c6fe64c26e84e18004aa05961f0dbae49a684ff5
Tobias Klein <contact@tklein.info>
2024-08-05 21:07:40 +0200

    Rename project to node-sword-web-api

--- routes/repository.js
+++ routes/repository.js
@@ -1,19 +1,19 @@
-/* This file is part of ezra-bible-app-server.
+/* This file is part of node-sword-web-api.
 
-   Copyright (C) 2024 ezra-bible-app-server Development Team <contact@ezrabibleapp.net>
+   Copyright (C) 2024 node-sword-web-api Development Team <contact@ezrabibleapp.net>
 
-   ezra-bible-app-server is free software: you can redistribute it and/or modify
+   node-sword-web-api is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 2 of the License, or
    (at your option) any later version.
 
-   ezra-bible-app-server is distributed in the hope that it will be useful,
+   node-sword-web-api is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with ezra-bible-app-server. See the file LICENSE.
+   along with node-sword-web-api. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
 const express = require('express');
